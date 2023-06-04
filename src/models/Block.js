c43aa977dab6ac746f1f4a861d9d381083854134
XRickey <tasaki0911@outlook.com>
2023-06-04 17:46:16 +0800

    update
    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,35 @@
-import sha256 from 'crypto-js/sha256.js'
+import UTXOPool from './UTXOPool.js'
 
-export const DIFFICULTY = 2
 
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(blockchain, parentHash, height, hash, miner) {
+    this.blockchain = blockchain
+    this.parentHash = parentHash
+    this.height = height
+    this.hash = hash
+    this.coinbaseBeneficiary = miner
+    this.utxoPool = new UTXOPool({})
+  }
+  getPreviousBlock () {
+    // 判断是否为高度为1的区块
+    if (this.height == 1) {
+      return this.blockchain.genesis
+    }
+    return this.blockchain.blocks[this.parentHash]
+  }
+  isValid () {
+    const leadingZero = '0'.repeat(DIFFICULTY)
 
-  isValid() {}
-
-  setNonce(nonce) {}
+    return (this.nonce + "").startsWith(leadingZero)
+  }
+  setNonce (nonce) {
+    this.nonce = nonce
+  }
 }
 
+  
+
 export default Block
+export const DIFFICULTY = 3
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,43 +1,60 @@
-import UTXOPool from './UTXOPool.js'
+import UTXO from "./UTXO.js"
 
-Blockchain
+// Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
+  /* 构造函数需要包含 
       - 名字
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
-
+  constructor(name) {
+    this.name = name
+    this.genesis = null
+    this.blocks = {}
+    if (this.genesis) {
+      this.blocks[this.genesis.hash] = this.genesis
+    }
+  }
   // 2. 定义 longestChain 函数
-  /*
+  /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-    return []
+  longestChain () {
+    let longestChain = []
+    for (let hash in this.blocks) {
+      let currBlock = this.blocks[hash]
+      let chain = [currBlock]
+      while (currBlock.parentHash != 'root' && currBlock.hash != 'root') {
+        currBlock = currBlock.getPreviousBlock()
+        chain.unshift(currBlock)
+      }
+      if (chain.length > longestChain.length) {
+        longestChain = chain
+      }
+    }
+    return longestChain
   }
-
-  // 判断当前区块链是否包含
-  containsBlock(block) {
-    // 添加判断方法
-    return false
+  //判断当前区块链是否包含
+  containsBlock (block) {
+    return this.blocks[block.hash] != null
   }
-
-  // 获得区块高度最高的区块
-  maxHeightBlock() {
-    // return Block
-  }
-
-  // 添加区块
-  /*
-
-  */
-  _addBlock(block) {
-    if (!block.isValid()) return
-    if (this.containsBlock(block)) return
-
-    // 添加 UTXO 快照与更新的相关逻辑
+  //添加区块
+  _addBlock (block) {
+    if (!block.isValid() || this.containsBlock(block)) {
+      return
+    } else {
+      //将新区块添加至blocks
+      this.blocks[block.hash] = block
+      //添加 UTXO 快照与更新相关逻辑
+      //复制新区块前一个区块交易池
+      var preUTXOPool = block.getPreviousBlock().utxoPool.clone()
+      block.utxoPool.utxos = preUTXOPool
+      //添加创币交易
+      let coinbaseUTXO = new UTXO(block.coinbaseBeneficiary, 12.5)
+      //在交易池添加该笔交易
+      block.utxoPool._addUTXO(coinbaseUTXO)
+    }
   }
 }
 
