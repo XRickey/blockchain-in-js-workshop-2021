faacd15a56589e22ffa3fe07562f9e32df094eb4
XRickey <tasaki0911@outlook.com>
2023-06-04 17:36:45 +0800

    update
    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,17 +1,28 @@
-import sha256 from 'crypto-js/sha256.js'
-
-export const DIFFICULTY = 2
-
 class Block {
   // 1. 完成构造函数及其参数
+  /* 构造函数需要包含
+   -名字
+   -创世区块
+   -存储区块的映射
+  */
+  constructor(blockchain, parentHash, height, hash) {
+    this.blockchain = blockchain
+    this.parentHash = parentHash
+    this.height = height
+    this.hash = hash
+  }
+  getPreviousBlock () {
+    return this.blockchain.blocks[this.parentHash]
+  }
+  isValid () {
+    const leadingZero = '0'.repeat(DIFFICULTY)
 
-  constructor() {}
-
-  isValid() {}
-
-  setNonce(nonce) {}
-  
+    return (this.nonce + "").startsWith(leadingZero)
+  }
+  setNonce (nonce) {
+    this.nonce = nonce
+  }
 }
 
 export default Block
-
+export const DIFFICULTY = 3
